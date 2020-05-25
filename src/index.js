0f1a4d1a26ae3f00a2d2a5cd7201aa5da885c068
Betuca <betuca69@gmail.com>
2020-05-25 11:07:56 -0300

    React-redux CRUD basic example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
@@ -18,3 +19,20 @@ ReactDOM.render(
     <Provider store={store}><App></App></Provider>,
     document.querySelector('#root')
 );
+=======
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+>>>>>>> b30d532... Initialize project using Create React App
