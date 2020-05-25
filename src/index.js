9758bc02b383e18b1597374f01f03e2b1841fca7
Betuca <betuca69@gmail.com>
2020-05-25 11:08:34 -0300

    React-redux CRUD basic example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
@@ -23,16 +24,31 @@ ReactDOM.render(
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+=======
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+>>>>>>> 72c27a8... React Stream client - basic CRUD example using react-redux
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from './components/App';
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(reduxThunk))
 );
 
+<<<<<<< HEAD
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 >>>>>>> b30d532... Initialize project using Create React App
+=======
+ReactDOM.render(
+    <Provider store={store}><App></App></Provider>,
+    document.querySelector('#root')
+);
+>>>>>>> 72c27a8... React Stream client - basic CRUD example using react-redux
